be6aac5dea68b02f9b8c253f6b5389d7546244c3
HLOEY <70062580+HLoey258@users.noreply.github.com>
2023-04-04 21:47:50 +0700

    pushing some stuff about scrollview/refreshControl

--- App.js
+++ App.js
@@ -1,24 +1,56 @@
-import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
-import { TailwindProvider } from "tailwindcss-react-native";
+import {
+  RefreshControl,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 export default function App() {
-  const [item, setItems] = useState([
+  const [items, setItem] = useState([
     { key: 1, item: "item 1" },
     { key: 2, item: "item 2" },
     { key: 3, item: "item 3" },
     { key: 4, item: "item 4" },
     { key: 5, item: "item 5" },
     { key: 6, item: "item 6" },
-    { key: 7, item: "item 7" },
+    { key: 8, item: "item 7" },
+    { key: 9, item: "item 7" },
+    { key: 10, item: "item 7" },
+    { key: 11, item: "item 7" },
+    { key: 12, item: "item 7" },
+    { key: 13, item: "item 7" },
+    { key: 14, item: "item 7" },
   ]);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const refresh = () => {
+    setRefreshing(true);
+    setItem([...items, { key: 60, item: "item 60" }]);
+    setRefreshing(false);
+    alert("refreshing");
+  };
   return (
-    <TailwindProvider>
-      <ScrollView>
-        <View id="wrapper" className="pt-10 items-stretch">
-          {}
-        </View>
-      </ScrollView>
-    </TailwindProvider>
+    <ScrollView
+      refreshControl={
+        <RefreshControl
+          onRefresh={refresh}
+          refreshing={refreshing}
+        ></RefreshControl>
+      }
+    >
+      <View id="wrapper" className="pt-10 items-stretch ">
+        {items.map((listItem) => {
+          return (
+            <Text
+              className="bg-slate-400 text-center m-4 p-6"
+              key={listItem.key}
+            >
+              {listItem.item}
+            </Text>
+          );
+        })}
+      </View>
+    </ScrollView>
   );
 }
