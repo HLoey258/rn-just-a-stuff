e66063232adc5633adad10e45f5d3b53fa36e671
HLOEY <70062580+HLoey258@users.noreply.github.com>
2023-04-04 10:06:21 +0700

    add item list

--- App.js
+++ App.js
@@ -1,34 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { TailwindProvider } from "tailwindcss-react-native";
 export default function App() {
+  const [item, setItems] = useState([
+    { key: 1, item: "item 1" },
+    { key: 2, item: "item 2" },
+    { key: 3, item: "item 3" },
+    { key: 4, item: "item 4" },
+    { key: 5, item: "item 5" },
+    { key: 6, item: "item 6" },
+    { key: 7, item: "item 7" },
+  ]);
   return (
     <TailwindProvider>
-      <View id="wrapper" className="pt-10 items-stretch">
-        <View id="top" className="flex-row justify-between ">
-          <Text className="p-5 items-center text-center bg-red-500 flex-2 ">
-            1
-          </Text>
-          <Text className="p-5 items-center text-center bg-green-500 flex-1">
-            2
-          </Text>
-          <Text className="p-5 items-center text-center bg-yellow-500 flex-1">
-            3
-          </Text>
+      <ScrollView>
+        <View id="wrapper" className="pt-10 items-stretch">
+          {}
         </View>
-        <View id="middle-top" className="flex-col justify-between ">
-          <Text className="p-5 items-center text-center bg-gray-500">4</Text>
-          <Text className="p-5 items-center text-center bg-blue-500">5</Text>
-        </View>
-        <View id="bottom" className="flex-row justify-between h-full">
-          <Text className="p-5 items-center text-center bg-white-500 flex-1">
-            6
-          </Text>
-          <Text className="p-5 items-center text-center bg-orange-500 flex-1">
-            7
-          </Text>
-        </View>
-      </View>
+      </ScrollView>
     </TailwindProvider>
   );
 }
