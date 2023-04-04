fb97d0c3d7b963e3d309bdb9bb50e9f59a231bab
HLOEY <70062580+HLoey258@users.noreply.github.com>
2023-04-04 09:57:55 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { TailwindProvider } from "tailwindcss-react-native";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TailwindProvider>
+      <View id="wrapper" className="pt-10 items-stretch">
+        <View id="top" className="flex-row justify-between ">
+          <Text className="p-5 items-center text-center bg-red-500 flex-2 ">
+            1
+          </Text>
+          <Text className="p-5 items-center text-center bg-green-500 flex-1">
+            2
+          </Text>
+          <Text className="p-5 items-center text-center bg-yellow-500 flex-1">
+            3
+          </Text>
+        </View>
+        <View id="middle-top" className="flex-col justify-between ">
+          <Text className="p-5 items-center text-center bg-gray-500">4</Text>
+          <Text className="p-5 items-center text-center bg-blue-500">5</Text>
+        </View>
+        <View id="bottom" className="flex-row justify-between h-full">
+          <Text className="p-5 items-center text-center bg-white-500 flex-1">
+            6
+          </Text>
+          <Text className="p-5 items-center text-center bg-orange-500 flex-1">
+            7
+          </Text>
+        </View>
+      </View>
+    </TailwindProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
