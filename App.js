91bc3b24dde35bac60c3be630132c41daf70894f
HLOEY <70062580+HLoey258@users.noreply.github.com>
2023-04-12 14:48:21 +0700

    use react navigate

--- App.js
+++ App.js
@@ -1,158 +1,64 @@
 import { useState } from "react";
 import {
-  Alert,
   Button,
   FlatList,
-  Modal,
   Pressable,
   RefreshControl,
   ScrollView,
   StyleSheet,
   Text,
   TextInput,
-  ToastAndroid,
   TouchableOpacity,
   View,
 } from "react-native";
-export default function App() {
-  const [text, setText] = useState("");
-  const [submit, setSubmit] = useState(false);
-  const [warning, setWarning] = useState(false);
+// to use react navigate npm this
+// npm install @react-navigation/native
+// depend on what are you using for react native project such as react native cli or expo
+// expo: npx expo install react-native-screens react-native-safe-area-context
+// reactnative cli: npm install react-native-screens react-native-safe-area-context
+// then go to react/stack
+// install: npm install @react-navigation/native-stack
+// IMPORT REACT NATIVE NAVIGATOR
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-  const handleSumbmitBtn = () => {
-    if (text.length > 3) {
-      setSubmit(!submit);
-    } else {
-      setWarning(!warning);
-      // ALERT COMPONENT
+const Stack = createNativeStackNavigator();
 
-      // Alert.alert( // the choice will be display form left to right
-      //   "Warning",
-      //   "You must enter at least 3 more characters",
-      //   [
-      //     {
-      //       text: "Do not",
-      //       onPress: () => {
-      //         console.warn("Do not pressed");
-      //       },
-      //     },
-      //     {
-      //       text: "OK",
-      //       onPress: () => {
-      //         console.warn("OK pressed");
-      //       },
-      //     },
-      //     {
-      //       text: "Cancel",
-      //       onPress: () => {
-      //         console.warn("Cancel pressed");
-      //       },
-      //     },
-      //   ],
-      //   {
-      //     cancelable: true, // this cancel able will allow you to close the toast message when click outside
-      //     onDismiss: () => {
-      //       console.warn("alert dimissed");
-      //     },
-      //   }
-      // );
-      // ToastAndroid.show("Hello this is a toast", 2000); // use the .show to present the toast 2000 is duration
-      // OR YOU CAN USE THIS
-      // ToastAndroid.show(
-      //   "This is a normal toast with short and long",
-      //   ToastAndroid.LONG // display for 3.5s
-      //   // ToastAndroid.SHORT // display for 2s
-      // );
-      // ToastAndroid.showWithGravity(
-      //   "hello this is a toast message with gravity",
-      //   2000,
-      //   ToastAndroid.CENTER // display the toast at center of the page
-      // );
-    }
-  };
-  const handleTextChange = (input) => {
-    return setText(input);
+const ScreenA = ({ navigation }) => {
+  const handleClick = () => {
+    navigation.navigate("ScreenB");
   };
   return (
-    <View className="mt-10 flex-1 items-center">
-      <Modal
-        visible={warning}
-        transparent
-        onRequestClose={() => {
-          // active when press back arrow in androoid
-          setWarning(false);
-        }}
-        animationType="fade" // can be fade or slide or...
-        hardwareAccelerated // use this prob to incease performance in android
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-lg text-black p-4">This is screen A</Text>
+      <Pressable
+        className="p-6 bg-orange-200 rounded-xl rounded-2"
+        onPress={handleClick}
       >
-        <View className="flex-1 justify-center items-center bg-[#00000099]">
-          <View
-            id="modal"
-            className="bg-white w-60 h-60  items-center border-2 rounded-xl"
-          >
-            <Text className="text-black text-lg mb-2 p-3 bg-yellow-300 w-full text-center rounded-t-lg ">
-              WARNING
-            </Text>
-            <Text className="text-black text-md text-center">
-              You must enter atleast more 3 characters
-            </Text>
-            <Pressable
-              onPress={() => {
-                setWarning(false);
-              }}
-            >
-              <Text className="top-20 p-2 bg-green-300 rounded-xl pl-5 pr-5">
-                OK
-              </Text>
-            </Pressable>
-          </View>
-        </View>
-      </Modal>
-      <Text className="text-center mb-3">Please input your name:</Text>
-      <TextInput
-        multiline // 2 line allowed
-        className="border-solid bg-slate-500 border-2 w-[40%] text-center rounded-md p-3 mb-5" // using text-center to center the value that you input
-        placeholder="Your name here..."
-        onChangeText={handleTextChange}
-        keyboardType="default" // find more in doc
-        maxLength={20}
-        // editable={false} // this probs will allowed/not you to edit text input
-        secureTextEntry={true} // turn text to dot (like password)
-      ></TextInput>
-
-      {/* BUTTON */}
-      <Button
-        title={submit ? "Clear" : "Sumbmit"} // In this component we only can use props to style the component
-        onPress={handleSumbmitBtn}
-        // disabled={submit}// disable or not the button
-        color={"gray"} // change button color
-      ></Button>
-
-      {/* TOUCHABLE */}
-      {/* <TouchableOpacity
-        onPress={handleSumbmitBtn}
-        className="p-3 bg-slate-500" // we can style for this component
-        activeOpacity={0.2} // set opacity when active
-      >
-        <Text>{submit ? "Clear" : "Sumbmit"}</Text>
-      </TouchableOpacity> */}
-
-      {/* PRESSABLE */}
-      {/* <Pressable
-        onLongPress={handleSumbmitBtn}
-        delayLongPress={2000}
-        // delayHoverIn={}
-        // delayHoverOut={}
-        // onHoverIn={}
-        // onHoverOut={}
-        // onPressIn={} onPressOut={}
-      >
-        <Text>sumbmit</Text>
-      </Pressable> */}
-
-      {submit ? (
-        <Text className="mt-3">You just inputdsafdsaf this: {text}</Text>
-      ) : null}
+        <Text>Change to screen B</Text>
+      </Pressable>
     </View>
+  );
+};
+
+const ScreenB = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-lg text-black p-4">This is screen B</Text>
+    </View>
+  );
+};
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          options={{ header: () => null }}
+          name="ScreenA"
+          component={ScreenA}
+        ></Stack.Screen>
+        <Stack.Screen name="ScreenB" component={ScreenB}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
